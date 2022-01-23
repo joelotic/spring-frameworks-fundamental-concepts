bd27db8b2c7eecd6a1b9ad6498129e3b5a2924f2
Joel <85992549+joelotic@users.noreply.github.com>
2022-01-23 11:29:15 +0800

    Add files via upload

---
+++ luliana-cosmina/chapter04/property-placeholder/src/main/java/com/apress/prospring5/ch4/AppProperty.java
@@ -0,0 +1,9 @@
+package com.apress.prospring5.ch4;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class AppProperty {
+    @Getter @Setter private String applicationHome;
+    @Getter @Setter private String userHome;
+}
